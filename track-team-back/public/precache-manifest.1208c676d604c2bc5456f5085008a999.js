a542f00d5f669f02c242cc97f0d901a91199178c
Ximena Perez <ximxim25@gmail.com>
2019-01-04 19:56:00 -0600

    yarn build

---
+++ track-team-back/public/precache-manifest.1208c676d604c2bc5456f5085008a999.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "affb0d4813a9a34106c7d287df2eaae8",
+    "url": "/static/media/banner2.affb0d48.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3842435067a45311ae48",
+    "url": "/static/js/main.38424350.chunk.js"
+  },
+  {
+    "revision": "0ef0d3d415b6d77f8c01",
+    "url": "/static/js/1.0ef0d3d4.chunk.js"
+  },
+  {
+    "revision": "3842435067a45311ae48",
+    "url": "/static/css/main.c8a5f96a.chunk.css"
+  },
+  {
+    "revision": "0ef0d3d415b6d77f8c01",
+    "url": "/static/css/1.af863b0c.chunk.css"
+  },
+  {
+    "revision": "0571f67adf02ff553bd9480e975a36ff",
+    "url": "/index.html"
+  }
+];
